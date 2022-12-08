4ea04d47b99dc9c391f1627ebb40a675c041f4a4
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-08 15:09:21 +0100

    Add documentation

--- core/src/main/java/com/github/sqyyy/jnb/Entrypoint.java
+++ core/src/main/java/com/github/sqyyy/jnb/Entrypoint.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to mark a method as an entrypoint.
+ *
+ * @since v0.1.0-alpha
+ */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.METHOD)
 public @interface Entrypoint {
