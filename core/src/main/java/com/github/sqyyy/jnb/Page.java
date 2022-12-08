5402b7b36af3561fd231523113a91f1030c66e21
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-08 15:09:39 +0100

    Add documentation and description to Page

--- core/src/main/java/com/github/sqyyy/jnb/Page.java
+++ core/src/main/java/com/github/sqyyy/jnb/Page.java
@@ -5,8 +5,28 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to mark a class as a notebook-page.
+ *
+ * @since v0.1.0-alpha
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Page {
+    /**
+     * The name of the page
+     *
+     * @return the name of the page
+     * @since v0.1.0-alpha
+     */
     String value();
+
+    /**
+     * The description of the page.
+     * An empty string by default.
+     *
+     * @return the description of the page
+     * @since v0.1.3-alpha
+     */
+    String description() default "";
 }
