c338a788cfdcdef71fa97648010905948e9de5af
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-08 20:58:35 +0100

    Add new utility methods to JavaNotebooks

--- core/src/main/java/com/github/sqyyy/jnb/JavaNotebooks.java
+++ core/src/main/java/com/github/sqyyy/jnb/JavaNotebooks.java
@@ -4,6 +4,11 @@ import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Java Notebooks access-point.
+ *
+ * @since v0.1.0-alpha
+ */
 @SuppressWarnings("unchecked")
 public class JavaNotebooks {
     private static final Class<?> entrypointsClass;
@@ -22,11 +27,57 @@ public class JavaNotebooks {
         }
     }
 
+    /**
+     * Gets the list of methods annotated with {@link Entrypoint}.
+     *
+     * @return the list of methods annotated with {@link Entrypoint}
+     * @since v0.1.0-alpha
+     */
     public static List<Method> getEntrypointMethods() {
         return Collections.unmodifiableList(entrypointMethods);
     }
 
+    /**
+     * Gets the list of methods annotated with {@link Entrypoint} in a given class.
+     *
+     * @param clazz the declaring class of the entrypoints
+     * @return the list of methods annotated with {@link Entrypoint} in a given class
+     * @since v0.1.4-alpha
+     */
+    public static List<Method> getEntrypointMethodsByClass(Class<?> clazz) {
+        return entrypointMethods.stream().filter(method -> method.getDeclaringClass() == clazz).toList();
+    }
+
+    /**
+     * Gets a list of the classes annotated with {@link Page}.
+     *
+     * @return an unmodifiable list of the classes annotated with {@link Page}
+     * @since v0.1.0-alpha
+     */
     public static List<Class<?>> getPageClasses() {
         return Collections.unmodifiableList(pageClasses);
+    }
+
+    /**
+     * Gets a list of the classes annotated with {@link Page} with a given page-name.
+     *
+     * @param name the name of the pages
+     * @return an unmodifiable list of the classes annotated with {@link Page} with a given page-name
+     * @since v0.1.4-alpha
+     */
+    public static List<Class<?>> getPageClassesByName(String name) {
+        return pageClasses.stream().filter(aClass -> aClass.getAnnotation(Page.class).value().equals(name)).toList();
+    }
+
+    /**
+     * Gets a list of the classes annotated with {@link Page} with a given page-name.
+     * Capitalization will be ignored.
+     *
+     * @param name the name of the pages
+     * @return an unmodifiable list of the classes annotated with {@link Page} with a given page-name
+     * @since v0.1.4-alpha
+     */
+    public static List<Class<?>> getPageClassesByNameIgnoreCase(String name) {
+        return pageClasses.stream().filter(aClass -> aClass.getAnnotation(Page.class).value().equalsIgnoreCase(name)).toList();
     }
 }
