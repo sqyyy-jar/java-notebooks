3aeb769dcba1fcce59abea66144310a393aa2fef
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-12 18:16:23 +0100

    Update examples

--- example/src/main/java/TestMain.java
+++ example/src/main/java/TestMain.java
@@ -2,20 +2,18 @@ import com.github.sqyyy.jnb.Entrypoint;
 import com.github.sqyyy.jnb.JavaNotebooks;
 import com.github.sqyyy.jnb.Page;
 
-import java.lang.reflect.InvocationTargetException;
-
 @Page(value = "Example-page", description = """
     This is an example-class testing the java notebooks annotation processing capabilities.
     """)
 public class TestMain {
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
-        for (var it : JavaNotebooks.getEntrypointMethods()) {
-            it.invoke(null, (Object) new String[0]);
+    public static void main(String[] args) throws Throwable {
+        for (var it : JavaNotebooks.getEntrypointHandles()) {
+            it.invoke((Object) new String[0]);
         }
     }
 
     @Entrypoint
-    public static void start(String... a) {
+    public static void start(String[] a) {
         System.out.println("hi");
     }
 }
