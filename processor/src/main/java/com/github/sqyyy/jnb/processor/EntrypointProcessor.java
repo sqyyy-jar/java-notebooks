dd255d59af02e49724c7169c7df81dd9a96f1345
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-12 18:14:57 +0100

    Improve Entrypoint API * entrypoint-metadata now stores a map of Class -> MethodHandle * migrate to new exceptions

--- processor/src/main/java/com/github/sqyyy/jnb/processor/EntrypointProcessor.java
+++ processor/src/main/java/com/github/sqyyy/jnb/processor/EntrypointProcessor.java
@@ -1,6 +1,9 @@
 package com.github.sqyyy.jnb.processor;
 
 import com.github.sqyyy.jnb.Entrypoint;
+import com.github.sqyyy.jnb.Page;
+import com.github.sqyyy.jnb.processor.error.IllegalSignatureException;
+import com.github.sqyyy.jnb.processor.error.MissingAnnotationException;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
@@ -8,7 +11,6 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.*;
-import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.ElementFilter;
 import java.util.ArrayList;
@@ -26,49 +28,65 @@ public class EntrypointProcessor extends AbstractProcessor {
         final List<ExecutableElement> entrypointExecutables = ElementFilter.methodsIn(entrypointElements);
         for (final ExecutableElement method : entrypointExecutables) {
             final TypeElement type = (TypeElement) method.getEnclosingElement();
+            final Name typeName = type.getQualifiedName();
+            final Name methodName = method.getSimpleName();
+            if (typeName.isEmpty()) {
+                throw new IllegalSignatureException("Type is an anonymous class");
+            }
+            if (methodName.isEmpty()) {
+                throw new IllegalSignatureException("Method is an anonymous method");
+            }
+            final Page annotation = type.getAnnotation(Page.class);
+            if (annotation == null) {
+                throw new MissingAnnotationException('@' + Page.class.getName() + " is missing on '" + typeName + '\'');
+            }
             if (!type.getModifiers().contains(Modifier.PUBLIC)) {
-                throw new RuntimeException(
-                    new IllegalAccessException("Entrypoint-class '" + type.getQualifiedName() + "' must be public"));
+                throw new IllegalSignatureException("Entrypoint-class '" + typeName + "' must be public");
             }
             if (!method.getModifiers().contains(Modifier.PUBLIC) || !method.getModifiers().contains(Modifier.STATIC)) {
-                throw new RuntimeException(
-                    new IllegalAccessException("Entrypoint '" + method.getSimpleName() + "' must be public and static"));
+                throw new IllegalSignatureException("Entrypoint '" + methodName + "' must be public and static");
             }
             final List<? extends VariableElement> parameters = method.getParameters();
             if (parameters.size() > 1) {
-                throw new RuntimeException(new IllegalAccessException(
-                    "Entrypoint '" + method.getSimpleName() + "' may only contain up to one parameter (args)"));
+                throw new IllegalSignatureException(
+                    "Entrypoint '" + methodName + "' may only contain up to one parameter (String[])");
             }
-            if (parameters.size() == 1) {
-                final TypeMirror argsParameter = parameters.get(0).asType();
-                processingEnv.getTypeUtils().isSameType(argsParameter, processingEnv.getTypeUtils()
-                    .getArrayType(processingEnv.getElementUtils().getTypeElement("java.lang.String").asType()));
-                if (argsParameter.getKind() != TypeKind.ARRAY) {
-                    throw new RuntimeException(new IllegalAccessException(
-                        "Entrypoint '" + method.getSimpleName() + "' may only contain string-array as parameter (args)"));
-                }
-                entrypointMethods.add(new JnbMethod(type.getQualifiedName().toString(), method.getSimpleName().toString(), true));
+            if (parameters.size() == 0) {
+                entrypointMethods.add(new JnbMethod(typeName.toString(), methodName.toString(), false));
                 continue;
             }
-            entrypointMethods.add(new JnbMethod(type.getQualifiedName().toString(), method.getSimpleName().toString(), false));
+            final TypeMirror argsParameter = parameters.get(0).asType();
+            if (!processingEnv.getTypeUtils().isSameType(argsParameter, processingEnv.getTypeUtils()
+                .getArrayType(processingEnv.getElementUtils().getTypeElement("java.lang.String").asType()))) {
+                throw new RuntimeException(new IllegalAccessException(
+                    "Entrypoint '" + methodName + "' may only contain string-array as parameter (String[])"));
+            }
+            entrypointMethods.add(new JnbMethod(typeName.toString(), methodName.toString(), true));
         }
         final StringBuilder metaFile = new StringBuilder();
-        metaFile.append("Class<?> var0 = null;\n");
+        metaFile.append("""
+            final MethodHandles.Lookup lookup = MethodHandles.publicLookup();
+            Class<?> var0 = null;
+            MethodHandle var1 = null;
+            """);
         for (final JnbMethod entrypointMethod : entrypointMethods) {
             metaFile.append("var0 = Class.forName(\"");
             metaFile.append(entrypointMethod.clazz());
-            metaFile.append("\");\n");
-            metaFile.append("$entrypoints.add(var0.getDeclaredMethod(\"");
+            metaFile.append("\");\nvar1 = lookup.findStatic(var0, \"");
             metaFile.append(entrypointMethod.method());
             metaFile.append('"');
             if (entrypointMethod.args()) {
-                metaFile.append(", String[].class");
+                metaFile.append(", MethodType.methodType(void.class, String[].class)");
             }
-            metaFile.append("));\n");
+            metaFile.append(");\n$entrypoints.put(var0, var1);\n");
         }
-        Processors.writeMetaClass(processingEnv.getFiler(), "Entrypoints", """
-                public static List<Method> $entrypoints = new ArrayList<>();""", metaFile.toString(), "java.util.List",
-            "java.util.ArrayList", "java.lang.reflect.Method");
+        Processors.writeMetaClass(processingEnv.getFiler(), "Entrypoints",
+            "public static final Map<Class<?>, MethodHandle> $entrypoints = new HashMap<>();", metaFile.toString(),
+            "java.lang.invoke.MethodHandle",
+            "java.lang.invoke.MethodHandles",
+            "java.lang.invoke.MethodType",
+            "java.util.HashMap",
+            "java.util.Map");
         return false;
     }
 }
